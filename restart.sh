7bfafa4833ea9f0e8cd432ded3a0f9be3b967384
FabricioSC <89610142+FabricioSC123@users.noreply.github.com>
2022-02-07 18:03:23 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,15 +2,6 @@
 # James Chambers - https://jamesachambers.com/minecraft-bedrock-edition-ubuntu-dedicated-server-guide/
 # Minecraft Bedrock Server restart script
 
-# Set path variable
-USERPATH="pathvariable"
-PathLength=${#USERPATH}
-if [[ "$PathLength" -gt 12 ]]; then
-    PATH="$USERPATH"
-else
-    echo "Unable to set path variable.  You likely need to download an updated version of SetupMinecraft.sh from GitHub!"
-fi
-
 # Check to make sure we aren't running as root
 if [[ $(id -u) = 0 ]]; then
    echo "This script is not meant to be run as root. Please run ./restart.sh as a non-root user, without sudo;  Exiting..."
